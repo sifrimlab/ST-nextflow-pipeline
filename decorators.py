26a786ab6ceb58c2c7eb907797997782a2824571
WoutDavid <wouters.david@hotmail.com>
2021-03-16 10:54:31 +0100

    fixed double running of a decorated function

--- decorators.py
+++ decorators.py
@@ -3,8 +3,8 @@ import time
 def measureTime(func):
     def wrapper(*args, **kwargs):
         starttime = time.perf_counter()
-        func(*args, **kwargs)
+        temp = func(*args, **kwargs)
         endtime = time.perf_counter()
         print(f"Time needed to run {func.__name__}: {endtime - starttime} seconds")
-        return(func(*args, **kwargs))
+        return(temp)
     return wrapper
