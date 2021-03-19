5c4bc4a6517ba6e6073c2c691929fa2100615bc5
WoutDavid <wouters.david@hotmail.com>
2021-03-19 16:21:33 +0100

    forgot to add one more nextflow file

---
+++ image_processing/registration/calculateTransform.py
@@ -0,0 +1,24 @@
+import SimpleITK as sitk
+import sys
+import os
+
+reference = sys.argv[1] 
+target=sys.argv[2]
+# output_dir=sys.argv[3]
+
+fixed = sitk.ReadImage(reference, sitk.sitkFloat32)
+moving = sitk.ReadImage(target, sitk.sitkFloat32)
+
+R = sitk.ImageRegistrationMethod()
+R.SetMetricAsCorrelation()
+R.SetOptimizerAsRegularStepGradientDescent(4.0, .01, 200)
+R.SetInitialTransform(sitk.TranslationTransform(fixed.GetDimension()))
+R.SetInterpolator(sitk.sitkLinear)
+outTx = R.Execute(fixed, moving)
+# print(f"Optimizer stop condition: {R.GetOptimizerStopConditionDescription()}")
+# print(f"Calculating transform of round {round}, channel {channel}...")
+# print(f"Finished at iteration {R.GetOptimizerIteration()} with a metric value of {R.GetMetricValue()}")
+# sitk.WriteTransform(outTx, f"transform_r{round}_c{channel}.txt")
+
+# print(str(reference) + str(target))
+print(outTx)
