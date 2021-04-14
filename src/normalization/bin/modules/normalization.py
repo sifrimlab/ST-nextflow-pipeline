changed the config behaviour so everything is in the correct place. Also implemented clip and rescale normalization

src/normalization/bin/modules/normalization.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 from skimage import io
 import cv2
 from icecream import ic
-image = "/home/david/Documents/image_registration/reference.tif"
-
-image = io.imread(image)
 
 def findCutOffPercentile(image_array, percent: int):
     flattened = image_array.flatten()
@@ -14,8 +11,8 @@ def findCutOffPercentile(image_array, percent: int):
     return cutoff_value
 
 def cutOffPercentile(image_array, percent: int):
-    cutoff_value = findCutOffPercentile(image, 90)
-    bool_array = np.where(image < cutoff_value, 0,image)
+    cutoff_value = findCutOffPercentile(image_array, percent)
+    bool_array = np.where(image_array < cutoff_value, 0,image_array)
     float_image = bool_array.astype(float)
     return float_image
 
@@ -29,9 +26,10 @@ def normalize(image_array):
 def clipAndNormalize(path_to_image: str, percent_to_clip: int, prefix=""):
     if not (0 <= percent_to_clip <100):
         raise Exception(f"Inputted percentage {percent_to_clip} is not between 0 and 100")
-    image = io.imread(image)
+    image = io.imread(path_to_image)
     cut_image = cutOffPercentile(image, percent=percent_to_clip)
-    io.imsave(f"{prefix}_normalized.tif")
+    return cut_image
+    io.imsave(f"{prefix}_normalized.tif", cut_image)
 
 
     