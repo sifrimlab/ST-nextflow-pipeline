d4ac4e674eb5b67233ccd2da1d7df36f70f6fb3a
WoutDavid <wouters.david@hotmail.com>
2021-05-02 21:49:59 +0200

    started adding my own assignment algorithm, first code

--- src/segmentation/bin/modules/assignGenes.py
+++ src/segmentation/bin/modules/assignGenes.py
@@ -18,13 +18,51 @@ def assignGenesToCells(labeled_image: str, decoded_genes: str, cell_properties: 
     decoded_df['Cell_Label'] = label_column
     return decoded_df
 
+def assignAllGenesToCells(labeled_image: str, decoded_genes: str, cell_properties: str, filter_unrecognized = True):
+    image = io.imread(labeled_image) # slicing = [Y,X]
+    decoded_df = pd.read_csv(decoded_genes)
+    cell_properties_df = pd.read_csv(cell_properties)
+    print(decoded_df.columns, cell_properties_df.columns)
+    if filter_unrecognized:
+        decoded_df = decoded_df[decoded_df['Gene'].isnull()!=True]
+
+    label_column = []
+
+    # This function finds the closest cell (from cell_properties_df)  to the given excerpt for the decoded_df 
+    def assignToNearestCell(decoded_df_row, cell_properties_df):
+        row_vector = np.array([decoded_df_row.X, decoded_df_row.Y])
+        closest_cell_label = 0
+        closest_cell_distance = np.inf
+        # distances_encountered = []
+        for cell in cell_properties_df.itertuples():
+            cell_vector = np.array([cell.Center_X, cell.Center_Y])
+            distance = np.linalg.norm(row_vector - cell_vector)
+            # distances_encountered.append(distance)
+            if distance < closest_cell_distance:
+                closest_cell_distance = distance
+                closest_cell_label = cell.Image_Label
+        # distances_encountered.sort() # sorts ascending
+        return closest_cell_label
+
+
+    # actually assign cells to rows
+    for row in decoded_df.itertuples():
+        label = image[row.Y, row.X]
+        if label == 0:
+            label = assignToNearestCell(row, cell_properties_df)
+
+        label = cell_properties_df.loc[cell_properties_df['Image_Label']==label, 'Cell_Label'].iloc[0]
+        label_column.append(label)
+    decoded_df['Cell_Label'] = label_column
+    return decoded_df
 
 if __name__=='__main__':
-    labeled_image = "/media/tool/gabriele_data/1442_OB/maxIP-seperate-channels/results_minsigma1_max_sigma15_stardist_segmentation_no_normalization/segmented/DAPI_padded_tiled_10_labeled.tif"
+    # labeled_image = "/media/tool/gabriele_data/1442_OB/maxIP-seperate-channels/results_minsigma1_max_sigma15_stardist_segmentation_no_normalization/segmented/DAPI_padded_tiled_10_labeled.tif"
     # labeled_image = io.imread(labeled_image)
-    decoded_genes ="/media/tool/gabriele_data/1442_OB/maxIP-seperate-channels/results_minsigma1_max_sigma15_stardist_segmentation_no_normalization/decoded/decoded_tiled_10.csv" 
-    df = pd.read_csv(decoded_genes)
-    filtered_df = assignGenesToCells(labeled_image, decoded_genes)
+    labeled_image="/media/david/Puzzles/starfish_test_data/MERFISH/seperate_stacks_cropped/results_tiled_whitetophat/segmented/MERFISH_nuclei_padded_tiled_3_labeled.tif" 
+    decoded_genes = "/media/david/Puzzles/starfish_test_data/MERFISH/seperate_stacks_cropped/results_tiled_whitetophat/decoded/decoded_tiled_3.csv"
+    cell_properties ="/media/david/Puzzles/starfish_test_data/MERFISH/seperate_stacks_cropped/results_tiled_whitetophat/segmented/MERFISH_nuclei_padded_tiled_3_properties.csv" 
+    filtered_df = assignAllGenesToCells(labeled_image, decoded_genes, cell_properties)
     print(filtered_df)
     # print(np.unique(labeled_image))
 
