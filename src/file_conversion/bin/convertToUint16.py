0c8e374b6ba9c70e2cca92bcdf8a72fcdc13f996
WoutDavid <wouters.david@hotmail.com>
2021-05-25 22:10:48 +0200

    added code to convert input images to uint16

---
+++ src/file_conversion/bin/convertToUint16.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from skimage import io
+from skimage.util import img_as_uint
+
+image_path = sys.argv[1]
+prefix = os.path.splitext(image_path)[0]
+image = io.imread(image_path)
+
+image_16bit = img_as_uint(image)
+io.imsave(f"{prefix}_16bit.tif", image_16bit)
